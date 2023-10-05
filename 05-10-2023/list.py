ba230652dc35219a5bcaa1535b0adf3d9a61f329
Nithakm <122859054+Nithakm@users.noreply.github.com>
2023-10-05 16:19:44 +0530

    Add files via upload

---
+++ 05-10-2023/list.py
@@ -0,0 +1,10 @@
+list=[32,43,12,-43,-65,-10,8,54,34,90,-88]
+new=[]
+print("The positive numbers are:")
+for i in list:
+    if i>0:
+        print(i)
+        new.append(i)
+print("new list is:",new)
+        
+        
---
+++ 05-10-2023/swap.py
@@ -0,0 +1,4 @@
+list=[1,4,5,7,8,9]
+print("current list:",list)
+list[0],list[-1]=list[-1],list[0]
+print("swapped list:",list)

d4883f9ae74db18aa6f413fbca12b1bd23440776
Nithakm <122859054+Nithakm@users.noreply.github.com>
2023-10-27 15:26:49 +0530

    Delete 26-10-2023 directory

--- 26-10-2023/remove1.py
+++
@@ -1,8 +0,0 @@
-list=[17,12,53,20,46,32]
-print("orginal list")
-print(list)
-for i in list:
-    if i%2==0:
-        list.remove(i)
-print("list after removing even number")
-print(list)
--- 26-10-2023/sorted.py
+++
@@ -1,7 +0,0 @@
-d={5:2,6:8,0:4,3:6,4:6}
-print("THE DICTIONARY:",d)
-sorted_d=sorted(d.items())
-print("DICTIONARY IN ASCENDING:",sorted_d)
-sorted_rev=sorted(d.items(),reverse=True)
-print("DICTIONARY IN DESCENDING:",sorted_rev)
-
